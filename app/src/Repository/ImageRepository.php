3448f7a18fdfae2e9ad855f4e143cc1e3f5f2e65
dd908090 <dolgopol.dima@gmail.com>
2025-05-16 23:55:21 +0300

    DEV-8: fix findById on ImageRepository.php

--- app/src/Repository/ImageRepository.php
+++ app/src/Repository/ImageRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Image;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ImageRepository
 {
@@ -20,8 +21,14 @@ class ImageRepository
         $this->entityManager->persist($image);
     }
 
-    public function getById(string $id): ?Image
+    public function findById(string $id): Image
     {
-        return $this->repo->find($id);
+        $image = $this->repo->find($id);
+
+        if (!$image) {
+            throw new NotFoundHttpException(sprintf('Image with ID "%s" not found', $id));
+        }
+
+        return $image;
     }
 }
